bddbe14a7c24441bb8dfcfd934d488d8adb5b2b9
metaphysical_light <metacat.me@gmail.com>
2024-03-20 08:34:54 +0300

    [Frontend] Added routing.

--- frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
 import { ReturnBook } from "./ReturnBook";
 import { SpinnerLoading } from "../../utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Carousel = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
@@ -127,9 +128,9 @@ export const Carousel = () => {
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">
-        <a className="btn btn-outline-secondary btn-lg" href="#">
+        <Link className="btn btn-outline-secondary btn-lg" to="/search">
           View More
-        </a>
+        </Link>
       </div>
     </div>
   );
